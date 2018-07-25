5f9a12778c9f6fd64c0a2f27820f9bade706032a
Bestin John <bestinjohn1993@gmail.com>
2018-07-25 08:09:00 +0530

    Added Description to Config file

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    API_KEY : '',
-    PORT:'',
-    SERVER_URL:''
+    API_KEY : '<ADD_YOUR_MAPBOX_API_KEY_HERE>',
+    PORT:'<ADD_YOUR_SERVER_PORT_HERE>',
+    SERVER_URL:'<ADD_YOUR_SERVER_IP_HERE>' //exempli gratia: (localhost, 127.0.0.1)
 };
 
