6bbb7680b33679a1052325ee22b71d63a4a583c7
Bestin John <bestii@users.noreply.github.com>
2018-08-03 09:15:04 +0530

    Updated Config.js
    To automatically capture port and hostname

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    API_KEY : '<ADD_YOUR_MAPBOX_API_KEY_HERE>',
-    PORT:'<ADD_YOUR_SERVER_PORT_HERE>',
-    SERVER_URL:'<ADD_YOUR_SERVER_IP_HERE>' //exempli gratia: (localhost, 127.0.0.1)
+    API_KEY : '<ADD_YOUR_MAPBOX_API_KEY_HERE>' ,
+    PORT: window.location.port ,
+    SERVER_URL: window.location.hostname //exempli gratia: (localhost, 127.0.0.1)
 };
 
